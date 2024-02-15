042e041141e40c2b6ecf0dfc2cd36d304ac6ca82
Temur <shernazarovtemurcom001@gmail.com>
2024-02-15 14:16:10 +0500

    some of them fixed

--- server/index.js
+++ server/index.js
@@ -15,7 +15,13 @@ import salesRoutes from "./routes/sales.js";
 import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
+import Transaction from "./models/Transaction.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+} from "./data/index.js";
 
 /*CONFIGURATION*/
 dotenv.config();
@@ -48,6 +54,7 @@ mongoose
 
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
     // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
